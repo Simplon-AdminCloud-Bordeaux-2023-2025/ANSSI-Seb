51237006e919f9741eb7af04d997e577fe71394f
Sebastienkusz <kusz.sebastien@gmail.com>
2024-04-04 10:58:24 +0200

    create venv

--- Terraform/anssi/ansible.tf
+++ Terraform/anssi/ansible.tf
@@ -3,7 +3,6 @@ resource "null_resource" "playbookconfig" {
   provisioner "local-exec" {
     working_dir = "${path.root}/../../Ansible"
     interpreter = ["bash", "-c"]
-    command     = "ansible-galaxy install -r requirements.yml -p roles/ --force"
     command     = "sleep 100; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook install-appli.yml -i inventaire.ini"
   }
 }
